e4e4ae3c43d5ab6ea20183c81bd7f109b757abf3
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-07 21:27:47 +0300

    add decorators in blog.py file

---
+++ blog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for,flash,redirect
+# from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='ed5627cbd9744f09982eea71e739332d'
+
+posts=[
+    {
+    'author':'Bettyb',
+    'title':'Blog Post 1',
+    'content':'First Post content',  
+    'date_posted': 'Feb 1,2020' 
+    },
+    {
+    'author':'Gakii seron',
+    'title':'Blog Post 2',
+    'content':'First Post content',  
+    'date_posted': 'Feb 3,2020' 
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
