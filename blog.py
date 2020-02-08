2c9d3d73a9800b7a886bd38c4bd5eec8e9f43e07
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-08 11:16:04 +0300

    created one to many relationship

--- blog.py
+++ blog.py
@@ -15,6 +15,7 @@ class User(db.model):
     image_file=db.Column(db.String(20),nullable=False,default='default.jpg')
     password=db.Column(db.String(60),nullable=False)
     posts=db.relationship('Post',backref='author,'lazy=True)
+    
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.username}')"
     
@@ -24,6 +25,7 @@ class Post(db.model):
     title==db.Column(db.String(100),nullable=False
     date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow())
     content=db.Column(db.Text,nullable=False)
+    user_id=db.Column(db.Interger,db.ForeignKey('user.id'),nullable=False)
     
     def __repr__(self):
         return f"User('{self.title}','{self.email}','{self.date_posted}')"
