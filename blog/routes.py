979e3feda747d43cce719bc8b2abce05360fa791
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-08 19:10:48 +0300

    created has password and commit it to db

--- blog/routes.py
+++ blog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from blog import app
+from blog import app, db,bcrypt  
 from blog.forms import RegistrationForm, LoginForm
 from blog.models import User, Post
 
@@ -35,6 +35,10 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user=User(username=form.username.data,email=form.email.data,password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
