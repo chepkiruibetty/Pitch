6209b6b4e456d8ed63139e41f907de0b906aedc7
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-08 15:22:06 +0300

    corrected on typos on blog.py

--- blog.py
+++ blog.py
@@ -1,46 +1,49 @@
 from datetime import datetime
-from flask import Flask,render_template, url_for,flash,redirect
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='0727325535'
+app.config['SECRET_KEY'] = '0727325535'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
+
 
 class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(120),unique=True,nullable=False)
-    email=db.Column(db.String(120),unique=True,nullable=False)
-    image_file=db.Column(db.String(20),nullable=False,default='default.jpg')
-    password=db.Column(db.String(60),nullable=False)
-    posts=db.relationship('Post',backref='author',lazy=True)
-    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
     def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.username}')"
-    
-    
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
 class Post(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(100),nullable=False)
-    date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-    content=db.Column(db.Text,nullable=False)
-    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
-    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
     def __repr__(self):
-        return f"User('{self.title}','{self.email}','{self.date_posted}')"
-        
-    
-posts=[
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+posts = [
     {
-    'title':'RIP President Moi',
-    'content':'The country will remember you for the great leadership',  
-    'date_posted': 'Feb ,7,2020' 
+        'author': 'Betty',
+        'title': 'RIP Moi',
+        'content': 'Death',
+        'date_posted': 'Feb 05, 2020'
     },
     {
-    'title':'Dev.to',
-    'content':'What I Learned about SEO from using JS frameworks',  
-    'date_posted': 'Feb 6,2020' 
+        'author': 'Vera',
+        'title': 'Python',
+        'content': 'The Zen',
+        'date_posted': 'Feb 03,2020'
     }
 ]
 
@@ -55,6 +58,7 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -62,6 +66,7 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
